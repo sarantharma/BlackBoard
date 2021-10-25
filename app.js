f8d2f9159299ff73bb3856d818002d3ec47a7c5b
Tharmarajah Saranyan <sarantharma7@gmail.com>
2021-10-25 19:16:20 -0400

    Add post request method to create a course

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const { toUSVString } = require("util");
 
 const app = express();
 
+// Middleware used to convert data to JSON format (Basically used in POST request)
+app.use(express.json());
+
 // Read the file and Reformate it
 const courses = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/courses-simple.json`)
@@ -17,6 +20,27 @@ app.get("/api/v1/courses", (req, res) => {
       courses,
     },
   });
+});
+
+app.post("/api/v1/courses", (req, res) => {
+  const newId = courses[courses.length - 1].id + 1;
+  // Object.assign create a new object by merging two existing objects together
+  console.log(req.body);
+  const newCourse = Object.assign({ id: newId }, req.body);
+
+  courses.push(newCourse);
+  fs.writeFile(
+    `${__dirname}/dev-data/courses-simple.json`,
+    JSON.stringify(courses),
+    (err) => {
+      res.status(201).json({
+        status: "success",
+        data: {
+          course: newCourse,
+        },
+      });
+    }
+  );
 });
 
 const port = 2000;
